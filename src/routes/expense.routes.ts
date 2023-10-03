ef54992a90e533a9a4c8efd7662091e04e5a7782
Parth442002 <parthkatiyar04@gmail.com>
2023-10-03 12:36:45 +0530

    Update Expense + isSettle + settlementDate

--- src/routes/expense.routes.ts
+++ src/routes/expense.routes.ts
@@ -1,4 +1,5 @@
 import express,{ Response} from 'express';
+import mongoose from "mongoose";
 require("dotenv").config();
 import ExpenseModel,{ExpenseDocument} from '../models/expenseModels';
 import { Request } from '../types/Request';
@@ -8,10 +9,11 @@ const router = express.Router();
 //Get all Expense were you are a payer or participant
 router.get("/",verifyToken,async(req:Request,res:Response)=>{
   try {
+    console.log(req.user.id)
     const expenses = await ExpenseModel.find({
       $or: [
-        { payer: req.user.user_id }, // User is the payer
-        { 'participants.user': req.user.user_id }, // User is one of the participants
+        { payer: req.user.id }, // User is the payer
+        { 'participants.user': req.user.id }, // User is one of the participants
       ],
     }).populate('payer participants.user', 'username');
     return res.status(200).send(expenses)
@@ -45,5 +47,37 @@ router.post("/",verifyToken,async(req:Request,res:Response)=>{
   }
 })
 
+//Update an Expense
+router.put("/:expenseId/",verifyToken,async(req:Request,res:Response)=>{
+  try {
+    const { expenseId } = req.params;
+    const updates = req.body; // Updated expense data
+
+    if (!mongoose.Types.ObjectId.isValid(expenseId)) {
+      return res.status(400).json({ error: 'Invalid expense ID' });
+    }
+
+    const updatedExpense = await ExpenseModel.findByIdAndUpdate(
+      expenseId,
+      updates,
+      { new: true } // Return the updated document
+    );
+
+    if (!updatedExpense) {
+      return res.status(404).json({ error: 'Expense not found' });
+    }
+    // Call the updateMeta function on each participant
+    for (const participant of updatedExpense.participants) {
+      participant.updateMeta();
+    }
+
+    updatedExpense.updateMeta();
+    await updatedExpense.save()
+    return res.status(201).send(updatedExpense)
+  } catch (error) {
+    console.log(error)
+    return res.status(400).send("Internal Server Error")
+  }
+})
 
 export default router
